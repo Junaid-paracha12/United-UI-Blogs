67131f759d2fbb629b4fa2c487c909e3f1ae4320
Junaid-paracha-007 <junaidmaqbool009@gmail.com>
2024-03-15 21:53:01 +0500

    uodate blogs

--- components/Blogs.js
+++ components/Blogs.js
@@ -16,10 +16,10 @@ export default function Blogs({title,btn1,btn2,data}) {
           <div key={i} className="flex flex-col gap-4">
             <Image src={v.imgsrc} width={5000} height={500} className="h-[40vh] rounded-2xl w-full object-fill" alt="img" />
             <div className="flex items-center gap-10">
-            <h1 className="text-xl font-bold">{v.title}</h1>
+            <h1 className="text-lg md:text-xl font-bold">{v.title}</h1>
             <Image src={v.iconsrc} width={20} height={20} alt="icon" className="" />
             </div>
-            <p className="text-sm font-semibold">{v.text}</p>
+            <p className="text-xs md:text-sm font-semibold">{v.text}</p>
               <div className="flex items-center gap-2">
               <Image src={v.imgsrc} width={50} height={50} className="w-7 h-7 rounded-full object-cover" alt="img" />
               <span className="flex items-center">
